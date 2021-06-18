5e9ed1baa45e08ed4953bcd1e4e814887f2070be
gadekarmd <gadekarmd929293@gmail.com>
2021-06-18 16:04:49 +0530

    all files are updated

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal");
+    if(principal.value<=0){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+    else{
+        principal = principal.value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = (principal * years * rate) /100;
+        var year = new Date().getFullYear() + parseInt(years);
+        document.getElementById("result").innerHTML="If you deposit <mark><b>"+principal+"</b></mark>, <br>at an interest rate of <mark><b>"+rate+"%</b></mark>. <br>You will receive an amount of <mark><b>"+interest+"</b></mark>, <br>in the year <mark><b>"+year+"</b></mark>.";    
+    }
+
 }
-        
+   
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    console.log(rateval)
+    document.getElementById("rate_val").innerText = rateval;
+
+}
